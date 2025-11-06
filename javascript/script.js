113d5b02753bc51ac031920b3790800fe3772617
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-11-06 15:47:46 +0100

    feat: inizio ad unire html e javascpirt

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,22 @@
 /* elementi necessari */
 const form = document.querySelector("form");
+const nomeInput = document.querySelector("#nome-cognome");
 const kmInput = document.querySelector("#chilometri");
-const etaInput = document.querySelector("#eta");
-const resalt = document.querySelector(".resalt");
+const etaInput = document.querySelector("#select");
+const resalt = document.querySelector(".card");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  const name = nomeInput.value.trim();
   const km = parseInt(kmInput.value);
   const eta = etaInput.value;
-  elaborazioneBiglietto(km, eta);
+
+  elaborazioneBiglietto(km, eta, name);
 });
 
-function elaborazioneBiglietto(km, eta) {
-  if (isNaN(km) || km <= 0) {
-    alert("Inserisci km");
+function elaborazioneBiglietto(km, eta, name) {
+  if (isNaN(km) || km <= 0 || name === "" || !isNaN(name)) {
+    alert("Inserisci input validi");
   } else {
     const prezzoKm = 0.21;
     const prezzoBiglietto = km * prezzoKm;
@@ -27,10 +30,24 @@ function elaborazioneBiglietto(km, eta) {
     }
     const risultatoFormattato = risultatoNum.toFixed(2);
 
-    risultatoInPagina(risultatoFormattato);
+    risultatoInPagina(risultatoFormattato, name);
   }
 }
 
-function risultatoInPagina(prezzo) {
-  resalt.innerHTML = ` Il prezzo del tuo biglietto è €${prezzo}`;
+function risultatoInPagina(prezzo, name) {
+  const nomePasseggero = name;
+  resalt.innerHTML = `<div class="card-header">
+                <h4 class="text-center">Nome Passeggero</h4>
+                <h6 class="text-center mt-3">${nomePasseggero}</h6>
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">Offerta</h5>
+                <p class="card-text">Biglietto standard</p>
+                <h5 class="card-title">Carrozza</h5>
+                <p class="card-text">5</p>
+                <h5 class="card-title">Codice cp</h5>
+                <p class="card-text">9000</p>
+                <h5 class="card-title">Costo biglietto</h5>
+                <p class="card-text">${prezzo}$</p>
+              </div>`;
 }
